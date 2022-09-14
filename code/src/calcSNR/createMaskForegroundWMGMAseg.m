7575a8d773e2316dd8018131a3f947ef1f369775
marcianunes <marcia.cg.nunes@gmail.com>
2022-09-14 22:24:59 +0100

    change filters and name of output masks from aseg

--- code/src/calcSNR/createMaskForegroundWMGMAseg.m
+++ code/src/calcSNR/createMaskForegroundWMGMAseg.m
@@ -11,10 +11,19 @@ BIDS = bids.layout(opt.dir.roi, 'use_schema', false);
 for subIdx = 1:numel(opt.subjects)
     
     subLabel = opt.subjects{subIdx};
-    aseg = spm_select('FPList', fullfile(opt.dir.roi, ['sub-' subLabel], 'ses-001/anat'), 'raseg.auto_noCCseg_interceptBrainmask.nii');
-
-    header_aseg = spm_vol(aseg);
-    aseg_vol = spm_read_vols(header_aseg);
+    %% find resliced aseg
+    
+    filter.modality = 'anat';
+    filter.sub = subLabel;
+    filter.ses = '001';
+    filter.acq = 'r0p75';
+    filter.suffix = 'aseg';
+    filter.prefix = 'r';
+    raseg = bids.query(BIDS, 'data', filter);
+    %raseg = spm_select('FPList', fullfile(opt.dir.roi, ['sub-' subLabel], 'ses-001/anat'), 'raseg.auto_noCCseg_interceptBrainmask.nii');
+    
+    header_raseg = spm_vol(char(raseg));
+    aseg_vol = spm_read_vols(header_raseg);
     
     %% white matter matrix
     label = 'WM';
@@ -24,33 +33,33 @@ for subIdx = 1:numel(opt.subjects)
 
     %Left WM
     wm_L(aseg_vol == 2) = 1;
-    WM_hemiLFilename = ['sub-' subLabel '_ses-001' '_hemi-L_space-individual_label-WM_desc-raseg.auto_noCCseg.nii']
+    WM_hemiLFilename = ['sub-' subLabel '_ses-001' '_hemi-L_space-individual_label-WM_raseg.nii']
 
-    header_wm_L = header_aseg;
-    header_wm_L.fname = fullfile(opt.dir.roi, ['sub-' subLabel], 'ses-001/roi', WM_hemiLFilename);
+    header_wm_L = header_raseg;
+    header_wm_L.fname = fullfile(opt.dir.roi, ['sub-' subLabel], 'ses-001', 'roi', WM_hemiLFilename);
     
     header_wm_L.private.dat.fname = header_wm_L.fname;
-    header_wm_L.dt = header_aseg.dt;
+    header_wm_L.dt = header_raseg.dt;
     wm_L_nii = spm_write_vol(header_wm_L, wm_L);    
     
     %Right WM
     wm_R(aseg_vol == 41) = 1;
     
-    WM_hemiRFilename = ['sub-' subLabel '_ses-001' '_hemi-R_space-individual_label-WM_desc-raseg.auto_noCCseg.nii']
-    header_wm_R = header_aseg;
-    header_wm_R.fname = fullfile(opt.dir.roi, ['sub-' subLabel], 'ses-001/roi', WM_hemiRFilename);
+    WM_hemiRFilename = ['sub-' subLabel '_ses-001' '_hemi-R_space-individual_label-WM_raseg.nii']
+    header_wm_R = header_raseg;
+    header_wm_R.fname = fullfile(opt.dir.roi, ['sub-' subLabel], 'ses-001', 'roi', WM_hemiRFilename);
     header_wm_R.private.dat.fname = header_wm_R.fname;
-    header_wm_R.dt = header_aseg.dt;
+    header_wm_R.dt = header_raseg.dt;
     wm_R_nii = spm_write_vol(header_wm_R, wm_R); 
     
     % save white matter 
     wm(aseg_vol == 2 | aseg_vol == 41) = 1;
-    WMFilename = ['sub-' subLabel '_ses-001' '_space-individual_label-WM_desc-raseg.auto_noCCseg.nii']
+    WMFilename = ['sub-' subLabel '_ses-001' '_space-individual_label-WM_raseg.nii']
 
-    header_wm = header_aseg;
-    header_wm.fname = fullfile(opt.dir.roi, ['sub-' subLabel], 'ses-001/roi', WMFilename);
+    header_wm = header_raseg;
+    header_wm.fname = fullfile(opt.dir.roi, ['sub-' subLabel], 'ses-001', 'roi', WMFilename);
     header_wm.private.dat.fname = header_wm.fname;
-    header_wm.dt = header_aseg.dt;
+    header_wm.dt = header_raseg.dt;
     wm_nii = spm_write_vol(header_wm, wm);    
     
     %% grey matter matrix
@@ -61,44 +70,44 @@ for subIdx = 1:numel(opt.subjects)
 
     %Left GM
     gm_L(aseg_vol == 3) = 1;
-    GM_hemiLFilename = ['sub-' subLabel '_ses-001' '_hemi-L_space-individual_label-GM_desc-raseg.auto_noCCseg.nii']
+    GM_hemiLFilename = ['sub-' subLabel '_ses-001' '_hemi-L_space-individual_label-GM_raseg.nii']
 
-    header_gm_L = header_aseg;
-    header_gm_L.fname = fullfile(opt.dir.roi, ['sub-' subLabel], 'ses-001/roi', GM_hemiLFilename);
+    header_gm_L = header_raseg;
+    header_gm_L.fname = fullfile(opt.dir.roi, ['sub-' subLabel], 'ses-001', 'roi', GM_hemiLFilename);
     
     header_gm_L.private.dat.fname = header_gm_L.fname;
-    header_gm_L.dt = header_aseg.dt;
+    header_gm_L.dt = header_raseg.dt;
     gm_L_nii = spm_write_vol(header_gm_L, gm_L);    
     
-    %Right WM
+    %Right GM
     gm_R(aseg_vol == 42) = 1;
     
-    GM_hemiRFilename = ['sub-' subLabel '_ses-001' '_hemi-R_space-individual_label-GM_desc-raseg.auto_noCCseg.nii']
-    header_gm_R = header_aseg;
-    header_gm_R.fname = fullfile(opt.dir.roi, ['sub-' subLabel], 'ses-001/roi', GM_hemiRFilename);
+    GM_hemiRFilename = ['sub-' subLabel '_ses-001' '_hemi-R_space-individual_label-GM_raseg.nii']
+    header_gm_R = header_raseg;
+    header_gm_R.fname = fullfile(opt.dir.roi, ['sub-' subLabel], 'ses-001', 'roi', GM_hemiRFilename);
     header_gm_R.private.dat.fname = header_gm_R.fname;
-    header_gm_R.dt = header_aseg.dt;
+    header_gm_R.dt = header_raseg.dt;
     gm_R_nii = spm_write_vol(header_gm_R, gm_R); 
     
     % save grey matter 
     gm(aseg_vol == 3 | aseg_vol == 42) = 1;
-    GMFilename = ['sub-' subLabel '_ses-001' '_space-individual_label-GM_desc-raseg.auto_noCCseg.nii']
+    GMFilename = ['sub-' subLabel '_ses-001' '_space-individual_label-GM_raseg.nii']
 
-    header_gm = header_aseg;
-    header_gm.fname = fullfile(opt.dir.roi, ['sub-' subLabel], 'ses-001/roi', GMFilename);
+    header_gm = header_raseg;
+    header_gm.fname = fullfile(opt.dir.roi, ['sub-' subLabel], 'ses-001', 'roi', GMFilename);
     header_gm.private.dat.fname = header_gm.fname;
-    header_gm.dt = header_aseg.dt;
+    header_gm.dt = header_raseg.dt;
     gm_nii = spm_write_vol(header_gm, gm);  
     
     
-    %create foreground - WM + GM
+    %% create foreground - WM + GM
     
     foreground = wm + gm;
-    ForegrdFilename = ['sub-' subLabel '_ses-001' '_space-individual_label-Foreground_desc-raseg.auto_noCCseg.nii']
-    header_Foregrd = header_aseg;
-    header_Foregrd.fname = fullfile(opt.dir.roi, ['sub-' subLabel], 'ses-001/roi', ForegrdFilename);
+    ForegrdFilename = ['sub-' subLabel '_ses-001' '_space-individual_label-foreground_raseg.nii']
+    header_Foregrd = header_raseg;
+    header_Foregrd.fname = fullfile(opt.dir.roi, ['sub-' subLabel], 'ses-001', 'roi', ForegrdFilename);
     header_Foregrd.private.dat.fname = header_Foregrd.fname;
-    header_Foregrd.dt = header_aseg.dt;
+    header_Foregrd.dt = header_raseg.dt;
     Foreground_nii = spm_write_vol(header_Foregrd, foreground);  
 
 end
